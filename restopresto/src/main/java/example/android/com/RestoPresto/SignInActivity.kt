a524626e8585d6f86728af7d8de3e95409962e70
r0ay1 <er_saidoune@esi..dz>
2018-06-19 17:29:45 +0100

    version avec ajout des activiés : sign in et log in 2

---
+++ restopresto/src/main/java/example/android/com/RestoPresto/SignInActivity.kt
@@ -0,0 +1,12 @@
+package example.android.com.RestoPresto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
